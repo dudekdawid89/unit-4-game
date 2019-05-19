29542a5f29fbe356f85c22b635f14b4143cc52cd
dudekdawid89 <dudekdawid89@gmail.com>
2019-05-18 20:33:22 -0500

    fully works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,13 @@
 var lossCounter = 0;
 var winCounter = 0;
 
-function startGame(){
+var counter = 0;
   var targetNumber = Math.floor(Math.random()*101)+19;
   $("#number-to-guess").text(targetNumber);
-
-  var counter = 0;
+  $('#counter').text('0');
+  $('#win').text('0');
+  $('#loss').text('0');
+ 
   var numberOptions = [
     Math.floor(Math.random()*11)+1, 
     Math.floor(Math.random()*11)+1,
@@ -27,6 +29,19 @@ function startGame(){
     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
     $("#crystals").append(imageCrystal);
   }
+  function startGame(){
+    targetNumber = Math.floor(Math.random()*101)+19;
+    numberOptions = [
+      Math.floor(Math.random()*11)+1, 
+      Math.floor(Math.random()*11)+1,
+      Math.floor(Math.random()*11)+1, 
+      Math.floor(Math.random()*11)+1
+    ];
+    $("#number-to-guess").text(targetNumber);
+    counter = 0;
+    $('#counter').text('0');
+
+  }
   $(".crystal-image").on("click", function() {
 
     var crystalValue = ($(this).attr("data-crystalvalue"));
@@ -39,18 +54,20 @@ function startGame(){
 
     if (counter === targetNumber) {
       winCounter++;
+      alert("WIN")
       $('#win').text(winCounter);
       startGame();
     }
 
     else if (counter >= targetNumber) {
       lossCounter++;
+      alert("LOSS")
       $('#loss').text(lossCounter);
       startGame();
     }
 
   });
-};
+
 
 
 
